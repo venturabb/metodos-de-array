21d7bda452b600c9a1b5da6fda0d83791df6b55f
FVS <felipe.ventura@bb.com.br>
2025-09-16 11:09:58 -0300

    implementado método forEach

---
+++ scripts/main.js
@@ -0,0 +1,100 @@
+let livros = [];
+const endpointDaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+
+async function getBuscarLivrosdaAPI() {
+  const resposta = await fetch(endpointDaAPI);
+  livros = await resposta.json();
+  return livros;
+  livros.forEach((livro) => {
+    console.table(livro);
+  });
+  // console.table(livros);
+  // new Promise((resolve, reject) => {});
+}
+
+async function exibirLivrosNaTela() {
+  const listaDeLivros = await getBuscarLivrosdaAPI();
+  const livrosNaPagina = document.getElementById("livros");
+
+  listaDeLivros.forEach((livro) => {
+    livrosNaPagina.innerHTML += `
+    <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+      ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">${livro.preco}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+        </div>
+    </div>`;
+  });
+}
+
+/* A estrutura é:
+
+<div class="livro">
+      <img class="livro__imagens indisponivel" src="" alt="" />
+      <h2 class="livro__titulo">Título</h2>
+      <p class="livro__descricao">Autor</p>
+      <p class="livro__preco" id="preco">Preço</p>
+      <div class="tags">
+        <span class="tag">Tag</span>
+        </div>
+    </div>
+    */
+
+async function gerarContainerLivro() {
+  const listaDeLivros = await getBuscarLivrosdaAPI();
+  const livrosNaPagina = document.getElementById("livros");
+
+  listaDeLivros.forEach((livro) => {
+    const containerNovoLivro = document.createElement("div");
+    containerNovoLivro.classList.add("livro");
+    const imagemLivro = document.createElement("img");
+    imagemLivro.classList.add("livro__imagens");
+    imagemLivro.src = livro.imagem;
+    const tituloLivro = document.createElement("h2");
+    tituloLivro.classList.add("livro__titulo");
+    tituloLivro.innerHTML = livro.titulo;
+    const autorLivro = document.createElement("p");
+    autorLivro.classList.add("livro__descricao");
+    autorLivro.innerHTML = livro.autor;
+    const precoLivro = document.createElement("p");
+    precoLivro.classList.add("livro__preco");
+    precoLivro.id = "preco";
+    precoLivro.innerHTML = livro.preco;
+    const tagsLivro = document.createElement("div");
+    tagsLivro.classList.add("tags");
+    const tagIndividual = document.createElement("span");
+    tagIndividual.classList.add("tag");
+    tagIndividual.innerHTML = livro.categoria;
+
+    tagsLivro.appendChild(tagIndividual);
+    containerNovoLivro.appendChild(imagemLivro);
+    containerNovoLivro.appendChild(tituloLivro);
+    containerNovoLivro.appendChild(autorLivro);
+    containerNovoLivro.appendChild(precoLivro);
+    containerNovoLivro.appendChild(tagsLivro);
+
+    // console.log(containerNovoLivro);
+
+    livrosNaPagina.appendChild(containerNovoLivro);
+  });
+}
+
+function rodaFuncaoDeFormaAleatoria() {
+  condicao = Math.random() < 0.5;
+  console.log(condicao);
+  if (condicao) {
+    exibirLivrosNaTela();
+    console.log("```````");
+  } else {
+    gerarContainerLivro();
+    console.log("appendChild");
+  }
+}
+
+rodaFuncaoDeFormaAleatoria();
